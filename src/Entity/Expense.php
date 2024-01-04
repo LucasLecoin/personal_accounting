530ea3f3c8841df417ca46256c2af6cc527073ad
Lucas Lecoin <lucaslecoin@gmail.com>
2024-01-04 10:04:04 +0100

    make:entity Expense

---
+++ src/Entity/Expense.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExpenseRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExpenseRepository::class)]
+class Expense
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $date = null;
+
+    #[ORM\Column]
+    private ?float $amount = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isCash = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isGain = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeImmutable $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isIsCash(): ?bool
+    {
+        return $this->isCash;
+    }
+
+    public function setIsCash(?bool $isCash): static
+    {
+        $this->isCash = $isCash;
+
+        return $this;
+    }
+
+    public function isIsGain(): ?bool
+    {
+        return $this->isGain;
+    }
+
+    public function setIsGain(?bool $isGain): static
+    {
+        $this->isGain = $isGain;
+
+        return $this;
+    }
+}
