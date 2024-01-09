8359a21e3b8b18d74f8bbfcebefed56d8547598e
Lucas Lecoin <lucaslecoin@gmail.com>
2024-01-09 10:29:19 +0100

    config i18n & root redirection

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,7 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    #[Route(path: '/', name: 'app_home')]
+    //config/routes.yaml : _app_root
+    public function __invoke(): Response
+    {
+        return $this->redirectToRoute('app_expense_index');
+    }
+
+    #[Route('/', name: 'app_home')]
     public function home(): Response
     {
         return $this->redirectToRoute('app_expense_index');
