98ec6933eb87b7efa7a1eb681747ec2ca2a8e8f6
Lucas Lecoin <lucaslecoin@gmail.com>
2024-01-09 10:29:38 +0100

    ExpenseController i18n

--- src/Controller/ExpenseController.php
+++ src/Controller/ExpenseController.php
@@ -7,11 +7,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(path: '/expenses', name: 'app_expense_')]
+#[Route(path: ['fr' => 'dépenses', 'en' => 'expenses'], name: 'app_expense_')]
 class ExpenseController extends AbstractController
 {
-    #[Route(path: '/', name: 'index', methods: [Request::METHOD_GET, Request::METHOD_POST])]
-    public function index(Request $request): Response
+    #[Route(path: ['fr' => '/', 'en' => '/'], name: 'index', methods: [Request::METHOD_GET, Request::METHOD_POST])]
+    public function index(): Response
     {
         return $this->render('app/expense/index.html.twig', [
             //TODO
