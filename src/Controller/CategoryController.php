e2d8efd35d796a418ef33f886191f1ba77bde263
Lucas Lecoin <lucaslecoin@gmail.com>
2024-01-10 12:15:01 +0100

    WIP CategoryController index

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,10 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: ['fr' => 'catégories', 'en' => 'categories'], name: 'app_category_')]
 class CategoryController extends AbstractController
 {
-
+    #[Route(path: ['fr' => '/', 'en' => '/'], name: 'index', methods: [Request::METHOD_GET, Request::METHOD_POST])]
+    public function index(): Response
+    {
+        return $this->render('app/category/index.html.twig', [
+            //TODO
+        ]);
+    }
 }
