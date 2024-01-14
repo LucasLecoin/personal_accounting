ecb9904e7fc06435dad63d2b244b3ba4393dc82d
Lucas Lecoin <lucaslecoin@gmail.com>
2024-01-14 23:16:57 +0100

    CategoryController new Category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,10 +14,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     #[Route(path: ['fr' => '/', 'en' => '/'], name: 'index', methods: [Request::METHOD_GET, Request::METHOD_POST])]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $category = new Category();
+        $formAdd = $this->createForm(CategoryType::class, $category, [
+            'method' => Request::METHOD_POST
+        ])->handleRequest($request);
+        if ($formAdd->isSubmitted() && $formAdd->isValid()) {
+            $em->persist($category);
+            $em->flush();
+
+//            $this->addFlash('notice', 'flash_message.category_added');
+            return $this->redirectToRoute('app_category_index');
+        }
+
         return $this->render('app/category/index.html.twig', [
-            //TODO
+            'formAdd' => $formAdd
         ]);
     }
 
