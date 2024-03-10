426fcb5fb00f24ddf462d2329d7207efdfc9fd3e
Lucas Lecoin <lucaslecoin@gmail.com>
2024-03-10 15:38:35 +0100

    FEAT FilterCategory object

---
+++ src/Filters/FilterCategory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filters;
+
+final class FilterCategory
+{
+    private ?\DateTimeInterface $start = null;
+    private ?\DateTimeInterface $end = null;
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->start;
+    }
+
+    public function setStart(?\DateTimeInterface $start): self
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getEnd(): ?\DateTimeInterface
+    {
+        return $this->end;
+    }
+
+    public function setEnd(?\DateTimeInterface $end): self
+    {
+        $this->end = $end;
+
+        return $this;
+    }
+}
