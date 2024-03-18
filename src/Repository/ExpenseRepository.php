e4b344d1c514643afcd770422dfc11881f84adff
Lucas Lecoin <lucaslecoin@gmail.com>
2024-03-18 17:51:44 +0100

    FEAT expenseRepository applyFilterExpense

--- src/Repository/ExpenseRepository.php
+++ src/Repository/ExpenseRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Expense;
+use App\Filters\FilterExpense;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -24,10 +26,43 @@ class ExpenseRepository extends ServiceEntityRepository
     /**
      * @return Expense[]
      */
-    public function getTableData(): array
+    public function getTableData(FilterExpense $filter): array
     {
         $qb = $this->createQueryBuilder('e')
             ->orderBy('e.date', 'DESC');
-        return $qb->getQuery()->getResult();
+        return $this->applyFilterExpense($qb, $filter)
+            ->getQuery()->getResult();
+    }
+
+    private function applyFilterExpense(QueryBuilder $qb, FilterExpense $filter): QueryBuilder
+    {
+        if($filter->getStart()) {
+            $qb ->andWhere('e.date >= :start')
+                ->setParameter('start', $filter->getStart());
+        }
+        if($filter->getEnd()) {
+            $qb ->andWhere('e.date <= :end')
+                ->setParameter('end', $filter->getEnd());
+        }
+        if($filter->getCategory()) {
+            $qb ->andWhere('e.category = :category')
+                ->setParameter('category', $filter->getCategory());
+        }
+        if($filter->getMinAmount()) {
+            $qb ->andWhere('e.amount >= :minAmount')
+                ->setParameter('minAmount', $filter->getMinAmount());
+        }
+        if($filter->getMaxAmount()) {
+            $qb ->andWhere('e.amount <= :maxAmount')
+                ->setParameter('maxAmount', $filter->getMaxAmount());
+        }
+        if(!is_null($filter->getIsCash())) {
+            $qb->andWhere('e.isCash '. ($filter->getIsCash() ? '':'!') . '= 1');
+        }
+        if(!is_null($filter->getIsGain())) {
+            $qb->andWhere('e.isGain '. ($filter->getIsGain() ? '':'!') . '= 1');
+        }
+
+        return $qb;
     }
 }
